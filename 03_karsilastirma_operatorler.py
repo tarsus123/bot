1d8c306ad92f690cb3c797b86650f5fcb6352395
tarsus123 <afkytty@gmail.com>
2023-03-11 13:01:33 +0300

    Create 03_karsilastirma_operatorler.py

---
+++ 03_karsilastirma_operatorler.py
@@ -0,0 +1,14 @@
+# esit mi?
+x = 5
+X = 6
+y = 10
+print(x == y) #Cikti: False
+print(x == 5) #Cikti: True
+
+# esit degil mi?
+print(x != y) #Cikti : False
+
+# Kucuk mu?
+print(x<y)   #Cikti: True
+print(y<x)   #Cikti: False
+print("x + 6<y:," x = 6 > y) #Cikti False 
