178934c63407b12d9ad2bd50a7e12c4103115121
tarsus123 <afkytty@gmail.com>
2023-03-11 12:43:53 +0300

    Create 02_aritmetik_operatorler.py

---
+++ 02_aritmetik_operatorler.py
@@ -0,0 +1,58 @@
+"""
+
+Toplam: +
+Cikarma: -
+Carpma: *
+Bolme: /
+Tam bolme: //
+Mod alma%
+"""
+#toplama islemi
+a = 5
+b = 3
+c = a + b
+print(c) #cikti: 8
+
+#cikarma islemi
+d = 10
+e = 6
+f + d - e
+print(f)  #cikti: 4
+
+#carpma islemi
+g = 6
+h = 8
+1 = h+g
+print(1) #cikti: 48
+
+#bolme islemi
+j = 86
+k =  7
+l = j / k
+print(l) #cikti: 12.285714285714286
+
+#Tam bolme
+
+m = j // k
+print(m) # cikti 12
+n = -86
+o = n // k
+print(o)
+
+
+#Mod Alma islemi
+
+p = j % k
+print(j) #Cikti 2
+r = n % k
+print(r) # cikti: 5
+
+# Us alma Islemi
+
+s = 2
+t = 3
+v = s ** t
+print(u) # Cikti 8
+#Islem Onceligi
+print(5 + 4 ** 3) # Cikti 69
+print((5+4)** 3 ) # Cikti: 729 
